733221abf0c065b359cf643a0716da8dc127a17c
sstephanyy <128330097+sstephanyy@users.noreply.github.com>
2023-06-26 14:07:16 -0300

    Initial commit

---
+++ files.js
@@ -0,0 +1,25 @@
+// file manager in node js --- FIRST PROJECT
+
+const fs = require('fs'); //file system module
+const path = require('path');
+  
+// First - specyfing the files extensions....
+const images = ['.png', '.jpg', '.jpeg'];
+const documents = ['pdf', 'docx', 'doc'];
+const videos = ['mp4', 'mov'];
+
+//readdir - get  list of every file inside the folder...
+fs.readdir('./source', (err, files) => {
+  if (err) {
+    console.log(err);
+  } else {
+    files.forEach((file) => {
+      const filesExtention = path.extname(file).toLowerCase(); //path.extname allow you to extract specific components from file paths, such as file extensions.
+      if (filesExtention === images[1]) {
+        console.log('there is a .jpg file');
+      } else {
+        console.log('nothing here!');
+      }
+    });
+  }
+});
